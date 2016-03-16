9b01e7db5bee5e8165f86f801f1ee674eab810e1
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-16 15:31:28 +0000

    Challenge 23

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -7,14 +8,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
+    $player1 = Player.new(params[:player_1_name])
+    $player2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb :play
   end
 
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,6 +1,6 @@
 def sign_in_and_play
   visit('/')
-  fill_in(:player1, with: 'Adil')
-  fill_in(:player2, with: 'Nick')
+  fill_in(:player_1_name, with: 'Adil')
+  fill_in(:player_2_name, with: 'Nick')
   click_button 'submit'
 end
