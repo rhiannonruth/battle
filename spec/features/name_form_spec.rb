55facb2bb7efd66f15346978c035e6961cb78f13
NickMountjoy <nick.mountjoy98@gmail.com>
2016-03-16 11:51:06 +0000

    Challenge 20

--- spec/features/name_form_spec.rb
+++ spec/features/name_form_spec.rb
@@ -8,4 +8,14 @@ feature 'Form takes name' , :type => :feature do
     click_button 'submit'
     expect(page).to have_content("Adil v Nick")
   end
+
+  scenario 'Can see player 2 hit points' do
+    visit('/')
+    fill_in(:player1, with: 'Adil')
+    fill_in(:player2, with: 'Nick')
+    click_button 'submit'
+    expect(page).to have_content('Nick - 60 hit points')
+
+  end
+
 end
