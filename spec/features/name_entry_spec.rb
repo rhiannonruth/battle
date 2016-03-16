a1f2276c6b0c496c1f0383ae2d2491909d111aac
rhiannonruth <rhiannonruth@hotmail.co.uk>
2016-03-16 11:10:32 +0000

    Added passing test for viewing Player 2's HP

--- spec/features/name_entry_spec.rb
+++ spec/features/name_entry_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 require 'capybara'
 
-feature '#Start fight' do
-  scenario '1.0 Fill in names' do
+feature 'Start fight' do
+  scenario 'Fill in names' do
       visit('/')
-      fill_in 'player1', with: "Terry"
-      fill_in 'player2', with: "Ruff"
-      click_button 'Play MOTHERS!'
-
-      expect(page).to have_text("Terry VS Ruff - GAME ON!!!")
-
+      fill_in :player1, with: "Terry"
+      fill_in :player2, with: "Ruff"
+      click_button "Let's PLAY!"
+      expect(page).to have_content("Terry VS Ruff - GAME ON!!!")
+  end
+  scenario "Viewing Player 2's Hit Points" do
+      visit('/')
+      fill_in :player1, with: "Terry"
+      fill_in :player2, with: "Ruff"
+      click_button "Let's PLAY!"
+      expect(page).to have_content("Ruff: 100 Hit Points")
   end
 end
