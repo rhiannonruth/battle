fea63f8e37060c7a1b877deba570add87c280b90
Claudia Green <claudia@claudiag.me.uk>
2016-03-17 12:33:28 +0000

    Challenge 27 complete.

--- app.rb
+++ app.rb
@@ -26,6 +26,11 @@ class Battle < Sinatra::Base
     erb :attack1
   end
 
+  post '/switch' do
+    $game.switch
+    redirect '/play'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
